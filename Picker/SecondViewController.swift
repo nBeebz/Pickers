5b7e331846341cbad86f58cdfd5b7b1d8ae5813c
Nav Bhatti <bhatti.nav@gmail.com>
2015-02-16 13:41:27 -0800

    initial commit
    Signed-off-by: Nav Bhatti <bhatti.nav@gmail.com>

---
+++ Picker/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Picker
+//
+//  Created by Nav Bhatti on 2015-02-16.
+//  Copyright (c) 2015 Nav Bhatti. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
